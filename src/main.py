3717138265a8c2fad816c3da403d58fc0d37b0b7
Gabriel Ionescu <ionescuig@yahoo.com>
2024-09-30 20:08:30 +0100

    Add redirect to docs and test endpoint

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import RedirectResponse
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
+@app.get("/", include_in_schema=False)
+async def root():
+    """Redirect to the /docs page."""
+    return RedirectResponse(url="/docs", status_code=status.HTTP_303_SEE_OTHER)
+
+
+app.router.prefix = "/api"
+
+
+@app.get("/test")
+async def test():
+    """Test endpoint."""
     return {"message": "Hello World!"}
